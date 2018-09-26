9df0740aac634edf5c838f6579f2036e23432302
Phil <pshock13@live.com>
2018-09-26 07:19:36 -0400

    responsive udpates
    sidebar on phil.html is more responsive to smaller screens

--- contacts/phil/js/main.js
+++ contacts/phil/js/main.js
@@ -1,6 +1,6 @@
 function openTab(evt, tabName) {
     // Declare all variables
-    var i, tabcontent, tablinks;
+    var i, tabcontent, navButton;
 
     // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -8,10 +8,10 @@ function openTab(evt, tabName) {
         tabcontent[i].style.display = "none";
     }
 
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    // Get all elements with class="navButton" and remove the class "active"
+    navButton = document.getElementsByClassName("navButton");
+    for (i = 0; i < navButton.length; i++) {
+        navButton[i].className = navButton[i].className.replace(" active", "");
     }
 
     // Show the current tab, and add an "active" class to the link that opened the tab
