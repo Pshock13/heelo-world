5736f6cc9ccb351f5b0c9692d418156b472529ab
Phillip Shockley <pshock13@live.com>
2023-06-02 11:16:58 -0400

    :)

--- contacts/phil/bash-beginner-series/scripts/fibonacci.sh
+++ contacts/phil/bash-beginner-series/scripts/fibonacci.sh
@@ -3,6 +3,25 @@
 # calculates the nth number in
 # the fibonacci sequence
 
-f(){
-    
+#!/bin/bash
+read -p "Enter number greater than 3: " num
+if [ "$num" -lt 3 ]; then
+echo "Number must be greater than 3"
+exit
+fi
+echo -n "Fibonacci series for $num terms is: "
+fibo() {
+if [ "$1" -le 1 ]; then
+echo -n "$1"
+else
+echo -n $(( $(fibo $(($1 - 1)) ) + $(fibo $(($1 - 2)) ) ))
+fi
 }
+for (( i=0; i<$num; ++i)); do
+fibo $i
+# Print , and space between the numbers except the last one
+if [ "$i" -lt $(($num -1)) ]; then
+echo -n ", "
+fi
+done
+echo ""
